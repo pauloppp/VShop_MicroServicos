a8037d46effba51089f158abf5f80caddb35da78
pauloppp <apaulob.sk@gmail.com>
2022-07-11 14:44:02 -0300

    Correções/Ajustes diversos

--- VShop_MicroServico.CarrinhoAPI/Contexto/CarrinhoAppDbContext.cs
+++ VShop_MicroServico.CarrinhoAPI/Contexto/CarrinhoAppDbContext.cs
@@ -10,10 +10,10 @@ namespace VShop_MicroServico.CarrinhoAPI.Contexto
         public DbSet<CarrinhoItem> CarrinhoItems { get; set; }
         public DbSet<CarrinhoCabec> CarrinhoCabecs { get; set; }
 
-        //Fluent API
+        // Fluent API
         protected override void OnModelCreating(ModelBuilder mb)
         {
-            //Produto
+            // Produto
             mb.Entity<Produto>().HasKey(p => p.Id);
             mb.Entity<Produto>().Property(p => p.Id).ValueGeneratedNever();
             mb.Entity<Produto>().Property(p => p.Nome).HasMaxLength(100).IsRequired();
@@ -22,7 +22,7 @@ namespace VShop_MicroServico.CarrinhoAPI.Contexto
             mb.Entity<Produto>().Property(p => p.CategoryNome).HasMaxLength(100).IsRequired();
             mb.Entity<Produto>().Property(p => p.Preco).HasPrecision(12, 2);
 
-            //CarrinhoCabec
+            // CarrinhoCabec
             mb.Entity<CarrinhoCabec>().Property(cc => cc.UserId).HasMaxLength(255).IsRequired();
             mb.Entity<CarrinhoCabec>().Property(cc => cc.CouponCode).HasMaxLength(100);
         }
